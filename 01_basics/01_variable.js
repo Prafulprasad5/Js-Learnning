00b8668d0965062adbc80ae0605bdc77d8a11529
Prafulprasad5 <prafulprasad5@gmail.com>
2024-09-19 15:33:24 +0000

    01_va

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,6 @@
+const acountId = "234567890"
+let accountName = "Sing alonge"
+var accountEmail = "hjhj@google.com"
+accountCity = "Mumbai"
+
+console.table([acountId, accountName, accountEmail, accountCity])
